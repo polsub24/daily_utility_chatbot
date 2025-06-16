4877c3f419f1ed617c0e536cf09f73d4bd75dbec
Subham Sarkar <poltu2108@gmail.com>
2025-06-16 20:58:05 +0530

    iframe fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,21 @@ function App() {
     <div>
       <h1>Welcome to Our Chatbot Page</h1>
       <p>Ask us anything!</p>
+
+      {/* WotNot iframe */}
+      <div style={{ marginTop: '2rem', display: 'flex', justifyContent: 'center' }}>
+        <iframe
+          width="640"
+          height="480"
+          src="https://embed.wotnot.io/7CwCKtweaScY134423446108TUswz8JP/bot/5rTsucYKk9c91346546578758yZ0UZja?display_header=false&history_retention=false"
+          frameBorder="0"
+          title="WotNot Chatbot"
+          style={{ border: 'none', borderRadius: '10px' }}
+        ></iframe>
+      </div>
     </div>
   );
 }
 
 export default App;
+
