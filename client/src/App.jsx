9c4131581b8c5faf507fc5ed05575f722581c4c7
Subham Sarkar <poltu2108@gmail.com>
2025-06-16 20:53:57 +0530

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://app.wotnot.io/chat-widget/7CwCKtweaScY134423446108TUswz8JP.js';
-    script.defer = true;
-    document.body.appendChild(script);
-  }, []);
-
   return (
     <div>
       <h1>Welcome to Our Chatbot Page</h1>
