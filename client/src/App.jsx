262ccab23c2c5609ccca4737bd647f30b404945f
Subham Sarkar <poltu2108@gmail.com>
2025-06-16 20:30:54 +0530

    vite client generated

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Our Chatbot Page</h1>
+      <p>Ask us anything!</p>
+    </div>
+  );
+}
+
+export default App;
